070e12ad309f477e13a0b5bd222c361487f09347
刘泓磊 <liuhonglei@liuhongleideMacBook-Pro.local>
2020-09-16 14:37:10 -0400

    update

--- personal_portfolio/portfolio/models.py
+++ personal_portfolio/portfolio/models.py
@@ -3,4 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    title = " "
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images')
